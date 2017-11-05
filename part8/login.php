048e678ed940425fceb49aebcc03b740c6b80697
Bangjie <qq315035253@gmail.com>
2017-11-05 10:34:43 +0800

    Add files via upload

---
+++ part8/login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html >
+<head>
+  <meta charset="UTF-8">
+  <title>Crowdfunding: Login Form</title>
+  
+  
+  <link rel='stylesheet prefetch' href='http://netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css'>
+
+      <link rel="stylesheet" href="loginStyle.css">
+  
+</head>
+
+<body>
+    <div class="wrapper">
+    <form class="form-signin">       
+      <h2 class="form-signin-heading">Login</h2>
+      <input type="text" class="form-control" name="username" placeholder="Email Address" required="" autofocus="" />
+      <input type="password" class="form-control" name="password" placeholder="Password" required=""/>      
+        <h4><a href="registerPage.php">click here to register </a></h4>
+      <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>   
+    </form>
+  </div>
+  
+  
+</body>
+</html>
